f3f028b01db22feb5e5a1c9d959c23f204af2803
josse995 <josse995@gmail.com>
2021-05-08 12:48:47 +0200

    Added some tests

--- Backend/src/main/java/com/eurovision/exercise/Backend/services/CityService.java
+++ Backend/src/main/java/com/eurovision/exercise/Backend/services/CityService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,6 +27,39 @@ public class CityService {
     }
 
     public Page<City> getCitiesByPagination(int page, int size){
-        return cityRepo.findAll(PageRequest.of(page,size));
+        return cityRepo.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, "name")));
+    }
+
+    public List<Integer> getLargestSequenceOfIncreasingIds(){
+        return getLargestSequenceOfIncreasingIds(cityRepo.findAll(Sort.by(Sort.Direction.ASC, "name")));
+    }
+
+    public List<Integer> getLargestSequenceOfIncreasingIds(List<City> list){
+
+        int maxCount = 1,  maxStart = 0, currCount = 1, currStart = 0;
+        for (int i = 1; i < list.size(); ++i){
+
+            if(list.get(i).getId() >= list.get(i-1).getId()){
+                ++currCount;
+            }else{
+                if(currCount > maxCount){
+                    maxCount = currCount;
+                    maxStart = currStart;
+                }
+                currStart = i;
+                currCount = 1;
+            }
+        }
+
+        if(currCount > maxCount){
+            maxCount = currCount;
+            maxStart = currStart;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < maxCount; ++i){
+            result.add(list.get(i + maxStart).getId());
+        }
+        return result;
     }
 }
--- Backend/src/test/java/com/eurovision/exercise/Backend/BackendApplicationTests.java
+++ Backend/src/test/java/com/eurovision/exercise/Backend/BackendApplicationTests.java
@@ -1,12 +1,51 @@
 package com.eurovision.exercise.Backend;
 
+import com.eurovision.exercise.Backend.model.City;
+import com.eurovision.exercise.Backend.repo.CityRepo;
+import com.eurovision.exercise.Backend.services.CityService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private CityService cityService;
+
+	@MockBean
+	private CityRepo cityRepo;
+
+	@Test
+	void getLargestSequenceOfIncreasingIds() throws Exception{
+		List<City> list = new ArrayList<City>(){{
+			add(new City(1, "Avila"));
+			add(new City(10, "Barcelona"));
+			add(new City(2,"Ciudad Real"));
+			add(new City(3, "Frankfurt"));
+			add(new City(4, "Granada"));
+			add(new City(5, "Hamburg"));
+		}};
+
+		List<Integer> resultList = cityService.getLargestSequenceOfIncreasingIds(list);
+
+		List<Integer> expectedList = new ArrayList<Integer>(){{
+			add(2);
+			add(3);
+			add(4);
+			add(5);
+		}};
+
+		assertEquals(expectedList, resultList);
 	}
 }
